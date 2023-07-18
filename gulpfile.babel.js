397c604955941484d3c5328253f4062e810e2299
loiluong <138481467+loiluong-kobiton@users.noreply.github.com>
2023-07-18 16:42:03 +0700

    asdasd

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,3 @@
-// const gulp = require('gulp');
-// const eslint = require('gulp-eslint');
-// const sourcemaps = require('gulp-sourcemaps');
-
-
-// gulp.task('lint', () => {
-//   return gulp
-//     .src(['src/**/*.js']) 
-//     .pipe(eslint())
-//     .pipe(eslint.format());
-// });
-
 
 const gulp = require('gulp');
 const jshint = require('gulp-jshint');
